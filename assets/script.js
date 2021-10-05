fb34a2111b110bf32ac7275482f71a094c04d094
MahoMori <tr27e1104@gmail.com>
2021-10-05 16:55:01 -0700

    changed a few css

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,37 @@
 const bgm = document.getElementById("bgm")
+const vol = bgm.volume
+bgm.volume = vol - 0.5
+
 const bgmList = {
     "coffee_day": "assets/audio/coffee_day.mp3",
     "honey_lemon": "assets/audio/honey_lemon.mp3",
     "memory": "assets/audio/memory.mp3",
+    "parade": "assets/audio/parade.mp3",
     "rpg": "assets/audio/rpg.mp3",
     "you_and_me": "assets/audio/you_and_me.mp3"
 }
 
-document.querySelector(".cover-img").addEventListener("click", () => {
+const seleall = document.querySelectorAll(".cover-img")
+for(el of seleall) {
+    let playing = false
+    
+    el.addEventListener("click", function() {
 
-    if(!bgm.paused) {
-        bgm.pause()
-    }
-    bgm.play()
+        if(!playing) {
 
-})
+            if(!bgm.paused) {
+                bgm.pause()
+            }
+        
+            let key = this.alt
+            bgm.src = bgmList[key]
+
+            bgm.play()
+        
+            playing = true
+        } else {
+            bgm.pause()
+            playing = false
+        }
+    })
+}
