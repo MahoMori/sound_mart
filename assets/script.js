3eb08eb19eb741815852109b8d4e2f4b42a1c7cb
MahoMori <tr27e1104@gmail.com>
2021-10-05 16:46:33 -0700

    simplified js

--- assets/script.js
+++ assets/script.js
@@ -15,18 +15,3 @@ document.querySelector(".cover-img").addEventListener("click", () => {
     bgm.play()
 
 })
-
-
-// var audio = new Audio("audio/coffee_day.mp3")
-
-
-    // audio.src = "audio/honey_lemon.mp3"
-    // audio.play()
-
-//     var canPlay = ('' != audio.canPlayType('audio/mpeg'))
-
-// if(canPlay === true) {
-//     alert('yes')
-// } else {
-//     alert("no")
-// }
