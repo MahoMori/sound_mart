eac8637aa80aaa40f1d93f7ee910e8f50fef844e
MahoMori <tr27e1104@gmail.com>
2021-10-06 17:25:30 -0700

    changed how to play bgms

--- assets/script.js
+++ assets/script.js
@@ -2,32 +2,22 @@ const bgm = document.getElementById("bgm")
 const vol = bgm.volume
 bgm.volume = vol - 0.5
 
-// const bgmList = {
-//     "coffee_day": "assets/audio/coffee_day.mp3",
-//     "honey_lemon": "assets/audio/honey_lemon.mp3",
-//     "memory": "assets/audio/memory.mp3",
-//     "parade": "assets/audio/parade.mp3",
-//     "rpg": "assets/audio/rpg.mp3",
-//     "you_and_me": "assets/audio/you_and_me.mp3"
-// }
+const showcaseLi = document.querySelector("#showcase-list")
 
-const seleall = document.querySelectorAll(".cover-img")
-for(el of seleall) {
-    let playing = false
-    
-    el.addEventListener("click", function() {
+showcaseLi.addEventListener("click", function(e) {
+    if(e.target.tagName === "IMG") {
+        const jacket = e.target
 
-        if(!playing) {
-        
-            let title = this.alt
+        if(!jacket.classList.contains("playing")) {
+            let title = jacket.alt
             bgm.src = "assets/audio/" + title + ".mp3"
-
+    
             bgm.play()
-        
-            playing = true
+    
+            jacket.classList.add("playing")
         } else {
             bgm.pause()
-            playing = false
+            jacket.classList.remove("playing")
         }
-    })
-}
+    }
+})
